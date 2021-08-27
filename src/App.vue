2f15a0c0e0ae272b69e853654a8cae86e6f03415
Roney <roneyfreitas.rf@gmail.com>
2021-08-27 13:50:42 -0300

    feat - Created the created hook

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <h1>{{ titulo }}</h1>
 
     <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+      <li v-for="foto in fotos">
+        <img :src="foto.url" alt="foto.titulo">
       </li>
     </ul>
 
@@ -16,17 +16,16 @@ export default {
   data () {
     return {
       titulo: 'DevPic',
-      fotos: [
-          {
-            url: 'https://s2.glbimg.com/pxaJidSOUGeEmMJ4odoGC1kTrUI=/0x0:2256x1329/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2020/5/1/7HTwAyRAGrVmdJngszOA/valor-imagem-1-tecnologias-digitais-na-saude.jpg',
-            titulo: 'smartphone'
-          },
-          {
-            url: 'https://s2.glbimg.com/pxaJidSOUGeEmMJ4odoGC1kTrUI=/0x0:2256x1329/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_63b422c2caee4269b8b34177e8876b93/internal_photos/bs/2020/5/1/7HTwAyRAGrVmdJngszOA/valor-imagem-1-tecnologias-digitais-na-saude.jpg',
-            titulo: 'smartphone'
-          }
-      ] 
+      fotos: [] 
     }
+  },
+
+  //hook Created é capas de acessar os dados e eventos reativos que estão ativos. Os modelos e o DOM viryual ainda não foram montados nem renderizados. Ex. Consumir dados da API.  
+  created() {
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(foto => this.fotos = foto, err => console.log(err));
   }
 }
 </script>
