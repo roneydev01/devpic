d703fee4aead7d605c80f950360feee952415dfa
Roney Freitas <roneyfreitas.rf@hotmail.com>
2021-08-30 15:37:31 -0300

    feat - Created component Painel

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,24 @@
     <h1 class="centralizado">{{ titulo }}</h1>
     
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto in fotos">
+      <li class="lista-fotos-item" v-for= "foto in fotos" >
     
-        <div class="painel">
-          <h2 class="painel-titulo"> {{ foto.titulo }} </h2>
-          <div class="painel-conteudo">
-            <img class="imagem-responsiva" :src="foto.url" alt="foto.titulo">
-          </div>
-        </div>
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" alt="foto.titulo">
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue'
+
 export default {
+  components: {
+    'meu-painel' : Painel
+  },
+
   data () {
     return {
       titulo: 'DevPic',
@@ -56,26 +59,6 @@ export default {
   }
   .imagem-responsiva {
     width: 100%;
-  }
-
-   .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: #8bebc6;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
   }
 
 </style>
