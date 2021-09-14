243817c3efd4f0be00c9c9aa68b8c8c91da6c655
Roney Freitas <roneyfreitas.rf@hotmail.com>
2021-09-14 14:24:53 -0300

    feat - Added style transition

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="painel">
+        {{ message }}
         <h2 class="painel-titulo" @dblclick="visivel = !visivel"> {{ titulo }}</h2>
-        <div class="painel-conteudo" v-show="visivel">
-            <slot>
-            </slot>
-        </div>
+        <transition name="painel-fade">
+            <div class="painel-conteudo" v-show="visivel">
+                <slot>
+                </slot>
+            </div>
+        </transition>
     </div>
 </template>
 
@@ -15,6 +18,12 @@ export default {
     data() {
         return {
             visivel: true,
+        }
+    },
+
+    computed: {
+        message() {
+            return `Faça doble click no título.`
         }
     }
 }
@@ -44,5 +53,13 @@ export default {
 
     * {
         box-shadow: 5px 5px 5px;
+    }
+
+    .painel-fade-enter, .painel-fade-leave-active {
+        opacity: 0
+    }
+
+    .painel-fade-enter-active, .painel-fade-leave-active {
+        transition: opacity .4s
     }
 </style>
